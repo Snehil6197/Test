178602e39340f915e3881a93f0be8f44a04f26c8
snehil <snehil3111999@gmail.com>
2022-02-08 15:04:18 +0530

    2nd commit

---
+++ First.java
@@ -0,0 +1,20 @@
+public class First {
+    public static void main(String arr[]) {
+        byte a1 = 127;
+        short a2 = 32000;
+        int a3 = 1294967296;
+        long a4 = 3454545643673637363l;
+        float f1 = 356.5f;
+        double d1 = 2736721.2376723863;
+        char a5 = 'a';
+        String s1 = "JAVA";
+        System.out.println(a1);
+        System.out.println(a2);
+        System.out.println(a3);
+        System.out.println(a4);
+        System.out.println(a5);
+        System.out.println(f1);
+        System.out.println(d1);
+        System.out.println(s1);
+    }
+}
